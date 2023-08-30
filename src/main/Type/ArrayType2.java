c348690a11a3d63519e30bdc6aaf01ff71530224
PARK9898 <wjddjs21@naver.com>
2023-08-30 21:56:48 +0900

    "다차원 배열"

---
+++ src/main/Type/ArrayType2.java
@@ -0,0 +1,35 @@
+package main.Type;
+
+public class ArrayType2 {
+	public static void main(String[] args) {
+		//  이중 배열
+		boolean[][] dblBoolAry = new boolean[3][3];
+
+		int[][] dblIntAry = new int[][] {
+			//  ⭐️ 요소 배열의 크기가 다를 수 있음
+			//상위 행은 주소값이 들어간다 행이 집이라 생각하고 열이 구성원이라고 생각하면된다
+			{1, 2, 3},
+			{4, 5},
+			{6, 7, 8, 9},
+		};
+
+		//  삼중 배열
+		char[][][] trpChrAry = {
+			{{'자', '축'}, {'인', '묘'}},
+			{{'진', '사'}, {'오', '미'}},
+			{{'신', '유'}, {'술', '해'}},
+		};
+
+		int int1 = dblIntAry[0][1];
+		int int2 = dblIntAry[2][3];
+		int[] intAry = dblIntAry[1];
+
+		char[][] dblChrAry = trpChrAry[0];
+		char[] chrAry = dblChrAry[0];
+		char chr1 = chrAry[1];
+		char chr2 = trpChrAry[2][0][1];
+
+
+	}
+}
+
