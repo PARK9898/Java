f6c2b14e92cfab574efd77a875f232a25f4cb2d3
PARK9898 <wjddjs21@naver.com>
2023-08-31 23:35:00 +0900

    "Class 내 static 메소드"

---
+++ src/main/classStudy/staticClass2/main.java
@@ -0,0 +1,9 @@
+package main.classStudy.staticClass2;
+
+public class main {
+	public static void main(String[] args) {
+		YalcoChicken store1 = new YalcoChicken("판교");
+		YalcoChicken store2 = new YalcoChicken("강남");
+		YalcoChicken store3 = new YalcoChicken("제주");
+	}
+}
