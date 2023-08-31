898fb97d335fdf10fd0b1c9c7465f161c36b92e5
PARK9898 <wjddjs21@naver.com>
2023-08-31 23:35:19 +0900

    "Class 내 static 메소드 2"

---
+++ src/main/classStudy/staticClass3/main.java
@@ -0,0 +1,21 @@
+package main.classStudy.staticClass3;
+
+public class main {
+	public static void main(String[] args) {
+		Button button1 = new Button('1', 1);
+		Button buttonPlus = new Button('+', 3);
+		Button buttonClear = new Button('C', 2);
+
+		Button[] buttons = {button1, buttonPlus, buttonClear};
+
+		System.out.println(Button.mode);
+		for (Button button : buttons) { button.place(); }
+
+		//  연속으로 붙여넣어 실행해볼 것
+		Button.switchMode(); // 모든 모드가 Dark로 바뀐다 (한번에 바꾸기 위해서)
+		// 바탕화면의 모든 것을 한번에 바꿔야하니까 이런 부분에서 나중에 서비스 할 때 유용하게 쓰일 수 있겠네
+
+		System.out.println(Button.mode);
+		for (Button button : buttons) { button.place(); }
+	}
+}
