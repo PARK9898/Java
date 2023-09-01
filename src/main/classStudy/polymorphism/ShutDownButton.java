aae4c1f4b5610987f962686dcf17e3c06665daf0
PARK9898 <wjddjs21@naver.com>
2023-09-01 21:39:58 +0900

    "Class 다형성"

---
+++ src/main/classStudy/polymorphism/ShutDownButton.java
@@ -0,0 +1,16 @@
+package main.classStudy.polymorphism;
+
+import main.classStudy.polymorphism.Button;
+
+public class ShutDownButton extends Button {
+	public ShutDownButton() {
+		super("ShutDown"); // 💡 부모의 생성자 호출
+		// 부모의 생성자를 먼저 호출하고 자식을 추가
+	}
+
+	//  💡 부모의 메소드를 override 부모가 가진 같은 이름의 메소드를 자식이무저다르게 정의
+	@Override
+	public void func () {
+		System.out.println("프로그램 종료");
+	}
+}
