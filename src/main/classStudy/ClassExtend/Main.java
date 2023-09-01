1c37d2a21b96a7a2385a41ade61786811a712b47
PARK9898 <wjddjs21@naver.com>
2023-09-01 21:36:09 +0900

    "Class 상속(자식 클래스의 기능을 사용하려면 명시적 타입 변환해야함!)"

--- src/main/classStudy/ClassExtend/Main.java
+++ src/main/classStudy/ClassExtend/Main.java
@@ -9,5 +9,25 @@ public class Main {
 		dtStore1.takeDTOrder();
 		dtStore1.setDriveThruOpen(false);
 		dtStore1.takeDTOrder();
+
+
+		System.out.println("\n- - - - -\n");
+
+		YalcoChicken ycStores[] = {
+			new YalcoChicken(3, "판교"),
+			new YalcoChicken(17, "강남"),
+			new YalcoChickenDT(108, "철원"),
+
+		};
+		// 그냥 얄코치킨 형으로 들어가있음
+		for (YalcoChicken store : ycStores) {
+			if (store instanceof YalcoChickenDT) {
+				//  ⭐️ 자식 클래스의 기능을 사용하려면 명시적 타입 변환
+				((YalcoChickenDT) store).takeDTOrder();
+				//store.takeDTOrder() -> 이렇게 하면 오류가난다 이 store는 얄코치킨으로 보기떄문이다
+			} else {
+				store.takeHallOrder();
+			}
+		}
 	}
 }
