28f4de79da05a99d92754c9a03527e44fb612041
PARK9898 <wjddjs21@naver.com>
2023-09-01 21:15:12 +0900

    "Class 상속(부모 생성자 없을경우)"

---
+++ src/main/classStudy/classExtend2/Slime.java
@@ -0,0 +1,11 @@
+package main.classStudy.classExtend2;
+
+public class Slime {
+	protected double hp = 50;
+	protected int attack = 8;
+	protected double defense = 0.2;
+
+	public void attack (Slime enemy) {
+		enemy.hp -= this.attack * (1 - enemy.defense);
+	}
+}
