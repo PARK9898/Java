6841c6db13cd70bc1813056f0572c87858f83954
PARK9898 <wjddjs21@naver.com>
2023-09-01 20:41:47 +0900

    "Class Getter&Setter"

---
+++ src/main/classStudy/GetterSetter/Main.java
@@ -0,0 +1,14 @@
+package main.classStudy.GetterSetter;
+
+public class Main {
+	public static void main(String[] args) {
+		Product ballPen = new Product("볼펜", 1000);
+
+		ballPen.setName("삼색볼펜");
+		ballPen.setName("");
+
+		int ballPenPrice = ballPen.getPrice();
+
+		ballPen.setPrice(1500);
+	}
+}
