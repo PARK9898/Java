900a0a9fc632af21227c8fa07ab84861ba9ea7b2
PARK9898 <wjddjs21@naver.com>
2023-09-03 20:47:01 +0900

    "Record형 기본"

---
+++ src/main/classStudy/Record/ChildClass.java
@@ -0,0 +1,20 @@
+package main.classStudy.Record;
+
+public class ChildClass {
+	//  기존처럼 클래스로 작성해야 했다면...
+	public class ChildClass1 {
+		private final String name;
+		private final int birthYear;
+		private final Gender gender;
+
+		public ChildClass1(String name, int birthYear, Gender gender) {
+			this.name = name;
+			this.birthYear = birthYear;
+			this.gender = gender;
+		}
+
+		public String getName() { return name; }
+		public int getBirthYear() { return birthYear; }
+		public Gender getGender() { return gender; }
+	}
+}
